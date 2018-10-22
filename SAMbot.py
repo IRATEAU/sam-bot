cd2fc7369986df84ce13a2af621f2bed2ff94512
Daniel Parker <github@parkisecurity.com>
2018-10-22 19:44:28 +1000

    Fixed errors in Issue #1. Improved logging and added seperate error log.

--- SAMbot.py
+++ SAMbot.py
@@ -1,38 +1,77 @@
 #!/usr/bin/env python3
-import os
-import time
-import re
+import os, time, re, requests, logging, json, pyjokes, traceback
 from slackclient import SlackClient
 from pprint import pprint
-import requests
-import logging
+from logging.config import dictConfig
 from mispattruploader import *
-import urllib3
-import json
-import pyjokes
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-import os 
+
+
+### Configuration loading and logging enabled.
 dir_path = os.path.dirname(os.path.realpath(__file__))
 config_file = dir_path + '/config.json'
 # instantiate Slack client
+output_error_file = ""
 with open(config_file) as json_data_file:
-    data = json.load(json_data_file)
+	data = json.load(json_data_file)
 token=data["slack"]["SLACK_BOT_TOKEN"]
-slack_client = SlackClient(token)
-misp = misp_custom(data['misp']['url'], data['misp']['key'])
 if 'logging' in data:
-    if 'output_file' in data['logging']:
-        logging.basicConfig(
-            filename=data['logging']['output_file'],
-            level=logging.INFO, 
-            format='%(asctime)s %(message)s', 
-            datefmt='%d/%m/%Y %I:%M:%S %p')
+	if 'output_file' in data['logging']:
+		output_all_file = data['logging']['output_file']
+	else:
+		exit("Please include output_file in config")
+	if 'output_error_file' in data['logging']:
+		output_error_file = data['logging']['output_error_file']
 else:
-    logging.basicConfig(
-        filename=dir_path + '/SAMbot.log',
-        level=logging.INFO,
-        format='%(asctime)s %(message)s', 
-        datefmt='%d/%m/%Y %I:%M:%S %p')
+	output_all_file = dir_path + "/sambot.log"
+
+
+logging_config = dict(
+	version = 1,
+	formatters = {
+		'f': {'format':
+			  '%(asctime)s - %(name)s - %(levelname)s - %(message)s'}
+		},
+	handlers = {
+		'Stream': {'class': 'logging.StreamHandler',
+			  'formatter': 'f',
+			  'level': 'DEBUG'
+		},        
+		'file_all': {
+			'class': 'logging.handlers.RotatingFileHandler',
+			'level': 'DEBUG',
+			'formatter': 'f',
+			'filename': output_all_file,
+			'mode': 'a',
+			'maxBytes': 10485760,
+			'backupCount': 5,
+		},
+	},
+	root = {
+		'handlers': ['Stream', 'file_all'],
+		'level': 'DEBUG',
+		},
+)
+
+if output_error_file != "":
+	logging_config['handlers']['file_error'] = {
+				'class': 'logging.handlers.RotatingFileHandler',
+				'level': 'ERROR',
+				'formatter': 'f',
+				'filename': output_error_file,
+				'mode': 'a',
+				'maxBytes': 10485760,
+				'backupCount': 5,
+		}
+	logging_config['root']['handlers'].append('file_error')
+
+dictConfig(logging_config)
+logger = logging.getLogger('SAMbot')
+slack_client = SlackClient(token)
+logger.info("Slack client created")
+logger.info("Connecting to misp server")
+misp = misp_custom(data['misp']['url'], data['misp']['key'])
+logger.info("Connected to misp server successfully")
+
 # starterbot's user ID in Slack: value is assigned after the bot starts up
 starterbot_id = None
 
@@ -42,87 +81,99 @@ EXAMPLE_COMMAND = "Tell a joke"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
 def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
-    for event in slack_events:
-        if event["type"] == "message" and "subtype" in event:
-            if event["subtype"] == "file_share":
-                url = event["file"]["url_private_download"]
-                title = event["file"]["title"]
-                if title == "Untitled":
-                    strTitle = "#Warroom"
-                else:
-                    strTitle = "#Warroom " + title
-                headers = {'Authorization': 'Bearer '+token}
-                r = requests.get(url, headers=headers)
-                content = r.content.decode("utf-8")
-                e_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(event['event_ts'])))
-                e_title = e_time + " - " + strTitle
-                misp_response = misp.misp_send(0, content, e_title)
-                return misp_response, None, event["channel"], event["user"]
-        elif event["type"] == "message" and not "subtype" in event:
-            logging.debug("Caught new method")
-            user_id, message = parse_direct_mention(event["text"])
-            logging.debug("User_id: %s \n Message: %s" %(user_id, message))
-            logging.debug("starterbot_id : %s" %starterbot_id)
-            if user_id == starterbot_id:
-                return None, message, event["channel"], user_id
-    return None, None, None, None
+	"""
+		Parses a list of events coming from the Slack RTM API to find bot commands.
+		If a bot command is found, this function returns a tuple of command and channel.
+		If its not found, then this function returns None, None.
+	"""
+	for event in slack_events:
+		try:
+			#logger.debug(event)
+			if event["type"] == "message" and "files" in event:
+				for file in event["files"]: 
+					if file["mode"] == "snippet":
+						url = file["url_private_download"]
+						title = file["title"]
+						if title == "Untitled":
+							strTitle = "#Warroom"
+						else:
+							strTitle = "#Warroom " + title
+						headers = {'Authorization': 'Bearer '+token}
+						r = requests.get(url, headers=headers)
+						content = r.content.decode("utf-8")
+						e_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(event['event_ts'])))
+						e_title = e_time + " - " + strTitle
+						misp_response = misp.misp_send(0, content, e_title)
+						return misp_response, None, event["channel"], event["user"]
+			elif event["type"] == "message" and not "subtype" in event:
+				logger.debug("Caught new method")
+				user_id, message = parse_direct_mention(event["text"])
+				logger.debug("User_id: %s \n Message: %s" %(user_id, message))
+				logger.debug("starterbot_id : %s" %starterbot_id)
+				if user_id == starterbot_id:
+					return None, message, event["channel"], user_id
+		except:
+			error = traceback.format_exc()
+			respond_channel(error, event["channel"])
+	return None, None, None, None
 
 def respond(command, channel, user):
-    # This is where you start to implement more commands!
-    # Sends the response back to the channel
-    slack_client.api_call(
-        "chat.postEphemeral",
-        channel=channel,
-        text=command,
-        user=user
-    )
+	# This is where you start to implement more commands!
+	# Sends the response back to the channel
+	slack_client.api_call(
+		"chat.postEphemeral",
+		channel=channel,
+		text=command,
+		user=user
+	)
+
+def respond_channel(command, channel):
+	slack_client.api_call(
+		"chat.postMessage",
+		channel=channel,
+		text=command
+	)
 
 def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
-    matches = re.search(MENTION_REGEX, message_text)
-    # the first group contains the username, the second group contains the remaining message
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+	"""
+		Finds a direct mention (a mention that is at the beginning) in message text
+		and returns the user ID which was mentioned. If there is no direct mention, returns None
+	"""
+	matches = re.search(MENTION_REGEX, message_text)
+	# the first group contains the username, the second group contains the remaining message
+	return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 def tell_a_joke(command, channel, user):
-    """
-        Executes bot command if the command is known
-    """
-    # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+	"""
+		Executes bot command if the command is known
+	"""
+	# Default response is help text for the user
+	default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
 
-    # Finds and executes the given command, filling in response
-    response = None
-    # This is where you start to implement more commands!
-    if command.lower().startswith(EXAMPLE_COMMAND.lower()):
-        response = pyjokes.get_joke(category='all') + " This joke has been Brought to you by pyjokes."
-    # Sends the response back to the channel
-    slack_client.api_call(
-        "chat.postMessage",
-        channel=channel,
-        text=response or default_response
-    )
+	# Finds and executes the given command, filling in response
+	response = None
+	# This is where you start to implement more commands!
+	if command.lower().startswith(EXAMPLE_COMMAND.lower()):
+		response = pyjokes.get_joke(category='all') + " This joke has been Brought to you by pyjokes."
+	# Sends the response back to the channel
+	slack_client.api_call(
+		"chat.postMessage",
+		channel=channel,
+		text=response or default_response
+	)
 
 if __name__ == "__main__":
-    if slack_client.rtm_connect(with_team_state=False):
-        logging.info("Starter Bot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        starterbot_id = slack_client.api_call("auth.test")["user_id"]
-
-        while True:
-            misp_response, smartass, channel, user = parse_bot_commands(slack_client.rtm_read())
-            if misp_response:
-                respond(misp_response, channel, user)
-            elif smartass:
-                print(smartass)
-                tell_a_joke(smartass, channel, user)
-            time.sleep(RTM_READ_DELAY)
-    else:
-        logging.info("Connection failed. Exception traceback printed above.")
+	if slack_client.rtm_connect(with_team_state=False):
+		logger.info("SAMbot connected and running!")
+		# Read bot's user ID by calling Web API method `auth.test`
+		starterbot_id = slack_client.api_call("auth.test")["user_id"]
+		while True:
+			misp_response, smartass, channel, user = parse_bot_commands(slack_client.rtm_read())
+			if misp_response:
+				respond(misp_response, channel, user)
+			elif smartass:
+				print(smartass)
+				tell_a_joke(smartass, channel, user)
+			time.sleep(RTM_READ_DELAY)
+	else:
+		logger.info("Connection failed. Exception traceback printed above.")
--- mispattruploader.py
+++ mispattruploader.py
@@ -1,187 +1,204 @@
 #!/usr/bin/env python3
-import logging
-import re
+import logging, re, requests, argparse, pymisp, sys, time, traceback
 from defang import refang
-import requests
-import argparse
-import pymisp
 from pymisp import *
-import sys
-import time
 from pprint import pprint
 from urllib.parse import urlparse
 
-
 class misp_custom:
-    
-    def __init__(self, misp_url, misp_key):
-        try:
-            self.misp = pymisp.PyMISP(misp_url, misp_key, False, 'json')
-        except Exception as err:
-            sys.exit('Batch Job Terminated: MISP connection error - \n'+repr(err))
+	
+	def __init__(self, misp_url, misp_key):
+		try:
+			self.misp = pymisp.PyMISP(misp_url, misp_key, False, 'json')
+		except Exception as err:
+			sys.exit('Batch Job Terminated: MISP connection error - \n'+repr(err))
+		misp_logger = logging.getLogger('mispattruploader')
 
-    def submit_to_misp(self, misp, misp_event, misp_objects):
-        '''
-        Submit a list of MISP objects to a MISP event
-        :misp: PyMISP API object for interfacing with MISP
-        :misp_event: MISPEvent object
-        :misp_objects: List of MISPObject objects. Must be a list
-        '''
-        # go through round one and only add MISP objects
-        a = []
-        for misp_object in misp_objects:
-            logging.debug(misp_object)
-            if len(misp_object.attributes) > 0:
-                template_id = misp.get_object_template_id(misp_object.template_uuid)
-                _a = misp.add_object(misp_event.id, template_id, misp_object)
-                logging.debug(_a)
-                a.append(_a)
-        # go through round two and add all the object references for each object
-        b = []
-        for misp_object in misp_objects:
-            for reference in misp_object.ObjectReference:
-                _b =misp.add_object_reference(reference)
-                b.append(_b)
-        return a,b
+	def submit_to_misp(self, misp, misp_event, misp_objects):
+		'''
+		Submit a list of MISP objects to a MISP event
+		:misp: PyMISP API object for interfacing with MISP
+		:misp_event: MISPEvent object
+		:misp_objects: List of MISPObject objects. Must be a list
+		'''
+		# go through round one and only add MISP objects
+		a = []
+		for misp_object in misp_objects:
+			logging.debug(misp_object)
+			if len(misp_object.attributes) > 0:
+				template_id = misp.get_object_template_id(misp_object.template_uuid)
+				_a = misp.add_object(misp_event.id, template_id, misp_object)
+				logging.debug(_a)
+				a.append(_a)
+		# go through round two and add all the object references for each object
+		b = []
+		for misp_object in misp_objects:
+			for reference in misp_object.ObjectReference:
+				_b =misp.add_object_reference(reference)
+				b.append(_b)
+		return a,b
 
-    def get_comm_and_tags(self, strInput):
-        comment = None
-        str_comment = ""
-        tags = ["tlp:green"]
-        for line in strInput.splitlines():
-            if ("comment:" in line.lower()):
-                vals = line.split(":", 1)
-                comment = vals[1:]
-            elif ("tag:" in line.lower()):
-                vals = line.split(":", 1)
-                value = vals[1].strip().lower()
-                if "tlp" in value:
-                    tags.remove("tlp:green")
-                    vals_str = "tlp:"
-                    vals_split = vals[1].split(":")
-                    vals_str += vals_split[1]
-                    tags.append(vals_str)
-            elif ("type:" in line.lower()):
-                vals = line.split(":", 1)
-                value = vals[1].strip().lower()
-                if value == "phish":
-                    tag = "ir8:" + value
-                    tags.append(tag)
-                elif value == "malware":
-                    tag = "ir8:" + value
-                    tags.append(tag)
-                elif value == "bec/spam":
-                    tag = "ir8:" + value
-                    tags.append(tag)
-                elif value == "dump":
-                    tag = "ir8:" + value
-                    tags.append(tag)
-                elif (value == "apt") or (value == "APT"):
-                    tag = "ir8:" + value
-                    tags.append(tag)
-        if comment != None:
-            for c in comment:
-                str_comment += c
-        else:
-            str_comment = comment
-        return str_comment, tags
+	def get_comm_and_tags(self, strInput):
+		comment = None
+		str_comment = ""
+		tags = ["tlp:green"]
+		for line in strInput.splitlines():
+			if ("comment:" in line.lower()):
+				vals = line.split(":", 1)
+				comment = vals[1:]
+			elif ("tag:" in line.lower()):
+				vals = line.split(":", 1)
+				value = vals[1].strip().lower()
+				if "tlp" in value:
+					tags.remove("tlp:green")
+					vals_str = "tlp:"
+					vals_split = vals[1].split(":")
+					vals_str += vals_split[1]
+					tags.append(vals_str)
+			elif ("type:" in line.lower()):
+				vals = line.split(":", 1)
+				value = vals[1].strip().lower()
+				if value == "phish":
+					tag = "ir8:" + value
+					tags.append(tag)
+				elif value == "malware":
+					tag = "ir8:" + value
+					tags.append(tag)
+				elif value == "bec/spam":
+					tag = "ir8:" + value
+					tags.append(tag)
+				elif value == "dump":
+					tag = "ir8:" + value
+					tags.append(tag)
+				elif (value == "apt") or (value == "APT"):
+					tag = "ir8:" + value
+					tags.append(tag)
+		if comment != None:
+			for c in comment:
+				str_comment += c
+		else:
+			str_comment = comment
+		return str_comment, tags
 
 
-    def misp_send(self, strMISPEventID, strInput, strInfo):
-        # Establish communication with MISP
-        
-        # The main processing block.
-        objects=[]
-        str_comment, tags = self.get_comm_and_tags(strInput)
-        mispobj_email = pymisp.MISPObject(name="email")
-        mispobj_file = pymisp.MISPObject(name="file")
-        mispobj_files = {}
-        mispobj_domainip = pymisp.MISPObject(name="domain-ip")
-        url_no = 0
-        file_no = 0
-        mispobj_urls = {}
-        for line in strInput.splitlines():
-            if ("domain:" in line.lower()):
-                vals = line.split(":", 1)
-                mispobj_domainip.add_attribute("domain", value=vals[1].strip(), comment=str_comment)
-            elif ("ip:" in line.lower()):
-                vals = line.split(":", 1)
-                mispobj_domainip.add_attribute("ip", value=vals[1].strip(), comment=str_comment)
-            elif ("source-email:" in line.lower()) or ("email-source" in line.lower()) or ("from:" in line.lower()):
-                vals = line.split(":", 1)
-                mispobj_email.add_attribute("from", value = vals[1].strip(), comment=str_comment)
-            elif ("url:" in line.lower()) or (('kit:' in line.lower() or ('creds:' in line.lower())) and (('hxxp' in line.lower()) or ('http' in line.lower()))):
-                vals = line.split(":", 1)
-                url = vals[1].strip()
-                url = refang(url)
-                parsed = urlparse(url)
-                mispobj_url = pymisp.MISPObject(name="url")
-                mispobj_url.add_attribute("url", value=parsed.geturl(), category="Payload delivery", comment=str_comment)
-                if parsed.hostname:
-                    mispobj_url.add_attribute("host", value=parsed.hostname, comment=str_comment)
-                if parsed.scheme:
-                    mispobj_url.add_attribute("scheme", value=parsed.scheme, comment=str_comment)
-                if parsed.port:
-                    mispobj_url.add_attribute("port", value=parsed.port, comment=str_comment)
-                mispobj_urls[url_no] = mispobj_url
-                url_no += 1
-            elif ("sha1:" in line.lower()) or ("SHA1:" in line):
-                vals = line.split(":", 1)
-                mispobj_file.add_attribute("sha1", value=vals[1].strip(),comment=str_comment)
-            elif ("sha256:" in line.lower()) or ("SHA256:" in line):
-                vals = line.split(":", 1)
-                mispobj_file.add_attribute("sha256", value=vals[1].strip(), comment=str_comment)
-            elif ("md5:" in line.lower()) or ("MD5:" in line):
-                vals = line.split(":", 1)
-                mispobj_file.add_attribute("md5", value=vals[1].strip(), comment=str_comment)
-            elif ("subject:" in line.lower()): #or ("subject:" in line):
-                vals = line.split(":", 1)
-                mispobj_email.add_attribute("subject", value = vals[1].strip(), comment=str_comment)
-            elif ("hash|filename:" in line.lower()):
-                vals = line.split(":", 1)
-                val = vals[1].split("|")
-                l_hash = val[0]
-                l_filename = val[1]
-                l_mispobj_file = pymisp.MISPObject(name="file")
-                if len(re.findall(r"\b[a-fA-F\d]{32}\b", l_hash)) > 0:
-                    l_mispobj_file.add_attribute("md5", value=l_hash.strip(), comment=str_comment)
-                    l_mispobj_file.add_attribute("filename", value=l_filename.strip(), comment=str_comment)
-                    mispobj_files[file_no] = l_mispobj_file
-                elif len(re.findall(r'\b[0-9a-f]{40}\b', l_hash)) > 0:
-                    l_mispobj_file.add_attribute("sha1", value=l_hash.strip(), comment=str_comment)
-                    l_mispobj_file.add_attribute("filename", value=l_filename.strip(), comment=str_comment)
-                    mispobj_files[file_no] = l_mispobj_file
-                elif len(re.findall(r'\b[A-Fa-f0-9]{64}\b', l_hash)) > 0:
-                    l_mispobj_file.add_attribute("sha256", value=l_hash.strip(), comment=str_comment)
-                    l_mispobj_file.add_attribute("filename", value=l_filename.strip(), comment=str_comment)
-                    mispobj_files[file_no] = l_mispobj_file
-                file_no +=1
-        objects.append(mispobj_file)
-        objects.append(mispobj_email)
-        objects.append(mispobj_domainip)
-        for u_key, u_value in mispobj_urls.items():
-            objects.append(u_value)
-        for f_key, f_value in mispobj_files.items():
-            objects.append(f_value)
-        # Update timestamp and event
-    #   try:
-        event = self.misp.new_event(info=strInfo, distribution='0', analysis='2', threat_level_id='3', published=True)
-        misp_event = MISPEvent()
-        misp_event.load(event)
-        a,b = self.submit_to_misp(self.misp, misp_event, objects)
-        for tag in tags:
-            self.misp.tag(misp_event.uuid, tag)
-        self.misp.fast_publish(misp_event.id, alert=False)
-        misp_event = self.misp.get_event(misp_event.id)
-        response = misp_event
-        #for response in misp_event:            
-        if ('errors' in response and response['errors'] != None):
-            return ("Submission error: "+repr(response['errors']))
-        else:
-            if response['Event']['RelatedEvent']:
-                e_related = ""
-                for each in response['Event']['RelatedEvent']:
-                    e_related = e_related + each['Event']['id'] + ", " 
-                return "Created ID: " + str(response['Event']['id']) + "\nRelated Events: " + ''.join(e_related)
-            else:
-                return "Created ID: " + str(response['Event']['id'])
+	def misp_send(self, strMISPEventID, strInput, strInfo):
+		# Establish communication with MISP
+		
+		# The main processing block.
+		try:
+			objects=[]
+			#get comments and tags from string input
+			str_comment, tags = self.get_comm_and_tags(strInput)
+
+			#setup misp objects
+			mispobj_email = pymisp.MISPObject(name="email")
+			mispobj_file = pymisp.MISPObject(name="file")
+			mispobj_files = {}
+			mispobj_domainip = pymisp.MISPObject(name="domain-ip")
+			url_no = 0
+			file_no = 0
+			mispobj_urls = {}
+
+			#process input
+			for line in strInput.splitlines():
+				if ("domain:" in line.lower()): #Catch domain and add to domain/IP object
+					vals = line.split(":", 1)
+					mispobj_domainip.add_attribute("domain", value=vals[1].strip(), comment=str_comment)
+				elif ("ip:" in line.lower()): #Catch IP and add to domain/IP object
+					vals = line.split(":", 1)
+					mispobj_domainip.add_attribute("ip", value=vals[1].strip(), comment=str_comment)
+				elif ("source-email:" in line.lower()) or ("email-source" in line.lower()) or ("from:" in line.lower()): #Catch email and add to email object
+					vals = line.split(":", 1)
+					mispobj_email.add_attribute("from", value = vals[1].strip(), comment=str_comment)
+				elif ("url:" in line.lower()) or (('kit:' in line.lower() or ('creds:' in line.lower())) and (('hxxp' in line.lower()) or ('http' in line.lower()))): #Catch URL and add to URL object
+					vals = line.split(":", 1)
+					url = vals[1].strip()
+					url = refang(url)
+					parsed = urlparse(url)
+					mispobj_url = pymisp.MISPObject(name="url")
+					mispobj_url.add_attribute("url", value=parsed.geturl(), category="Payload delivery", comment=str_comment)
+					if parsed.hostname:
+						mispobj_url.add_attribute("host", value=parsed.hostname, comment=str_comment)
+					if parsed.scheme:
+						mispobj_url.add_attribute("scheme", value=parsed.scheme, comment=str_comment)
+					if parsed.port:
+						mispobj_url.add_attribute("port", value=parsed.port, comment=str_comment)
+					mispobj_urls[url_no] = mispobj_url
+					url_no += 1
+
+				#Catch different hashes and add to file object
+				elif ("sha1:" in line.lower()) or ("SHA1:" in line):
+					vals = line.split(":", 1)
+					mispobj_file.add_attribute("sha1", value=vals[1].strip(),comment=str_comment)
+				elif ("sha256:" in line.lower()) or ("SHA256:" in line):
+					vals = line.split(":", 1)
+					mispobj_file.add_attribute("sha256", value=vals[1].strip(), comment=str_comment)
+				elif ("md5:" in line.lower()) or ("MD5:" in line):
+					vals = line.split(":", 1)
+					mispobj_file.add_attribute("md5", value=vals[1].strip(), comment=str_comment)
+				elif ("subject:" in line.lower()): #or ("subject:" in line): #Catch subject and add to email object
+					vals = line.split(":", 1)
+					mispobj_email.add_attribute("subject", value = vals[1].strip(), comment=str_comment)
+				elif ("hash|filename:" in line.lower()): #catch hash|filename pair and add to file object
+					vals = line.split(":", 1)
+					val = vals[1].split("|")
+					l_hash = val[0]
+					l_filename = val[1]
+					l_mispobj_file = pymisp.MISPObject(name="file")
+					if len(re.findall(r"\b[a-fA-F\d]{32}\b", l_hash)) > 0:
+						l_mispobj_file.add_attribute("md5", value=l_hash.strip(), comment=str_comment)
+						l_mispobj_file.add_attribute("filename", value=l_filename.strip(), comment=str_comment)
+						mispobj_files[file_no] = l_mispobj_file
+					elif len(re.findall(r'\b[0-9a-f]{40}\b', l_hash)) > 0:
+						l_mispobj_file.add_attribute("sha1", value=l_hash.strip(), comment=str_comment)
+						l_mispobj_file.add_attribute("filename", value=l_filename.strip(), comment=str_comment)
+						mispobj_files[file_no] = l_mispobj_file
+					elif len(re.findall(r'\b[A-Fa-f0-9]{64}\b', l_hash)) > 0:
+						l_mispobj_file.add_attribute("sha256", value=l_hash.strip(), comment=str_comment)
+						l_mispobj_file.add_attribute("filename", value=l_filename.strip(), comment=str_comment)
+						mispobj_files[file_no] = l_mispobj_file
+					file_no +=1
+
+			#add all misp objects to List to be processed and submitted to MISP server as one.
+			objects.append(mispobj_file)
+			objects.append(mispobj_email)
+			objects.append(mispobj_domainip)
+			for u_key, u_value in mispobj_urls.items():
+				objects.append(u_value)
+			for f_key, f_value in mispobj_files.items():
+				objects.append(f_value)
+			# Update timestamp and event
+		except Exception as e:
+			error = traceback.format_exc()
+			response = "Error occured when converting string to misp objects:\n %s" %error
+			misp_logger.error(response)
+			return response
+
+		
+		try:
+			event = self.misp.new_event(info=strInfo, distribution='0', analysis='2', threat_level_id='3', published=True)
+			misp_event = MISPEvent()
+			misp_event.load(event)
+			a,b = self.submit_to_misp(self.misp, misp_event, objects)
+			for tag in tags:
+				self.misp.tag(misp_event.uuid, tag)
+			self.misp.fast_publish(misp_event.id, alert=False)
+			misp_event = self.misp.get_event(misp_event.id)
+			response = misp_event
+			#for response in misp_event:            
+			if ('errors' in response and response['errors'] != None):
+				return ("Submission error: "+repr(response['errors']))
+			else:
+				if response['Event']['RelatedEvent']:
+					e_related = ""
+					for each in response['Event']['RelatedEvent']:
+						e_related = e_related + each['Event']['id'] + ", " 
+					return "Created ID: " + str(response['Event']['id']) + "\nRelated Events: " + ''.join(e_related)
+				else:
+					return "Created ID: " + str(response['Event']['id'])
+
+		except Exception as e:
+			error = traceback.format_exc()
+			response = "Error occured when submitting to misp:\n %s" %error
+			misp_logger.error(response)
+			return response
